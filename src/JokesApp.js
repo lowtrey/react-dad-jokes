8b7bcd006762102cfb992ea002ebac3d8a559581
Q. Price <priceqp@gmail.com>
2019-07-02 19:37:27 -0500

    Create initial layout and add state to JokesApp component

---
+++ src/JokesApp.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class JokesApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return(
+      <div>
+        <h1>Jokes App Here</h1>
+      </div>
+    );
+  }
+}
+
+export default JokesApp;
