8c7c630b2fa13ed37626b61b61d6463bb07bdeda
Q. Price <priceqp@gmail.com>
2019-07-03 11:25:57 -0500

    Add styling to components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import JokeList from './JokeList';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <JokeList />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <JokeList />
+      </div>
+    );
+  }
 }
 
 export default App;
