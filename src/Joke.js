13979e8cdb814e2815036c01f6b9839c998cbb2d
Q. Price <priceqp@gmail.com>
2019-07-03 11:51:02 -0500

    Add fontawesome, create Joke component

---
+++ src/Joke.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Joke extends React.Component {
+  render() {
+    return(
+      <div className='Joke'>
+        <div className='Joke-buttons'>
+          
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Joke;
