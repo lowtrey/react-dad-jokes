e2a177d108c67cb69b215eabf10cbc16a836221f
Q. Price <priceqp@gmail.com>
2019-07-03 20:27:45 -0500

    Add and style joke loading spinner

--- src/JokeList.js
+++ src/JokeList.js
@@ -10,7 +10,9 @@ class JokeList extends React.Component {
   };
   constructor(props) {
     super(props);
-    this.state = { jokes: JSON.parse(window.localStorage.getItem('jokes') || '[]') };
+    this.state = { jokes: JSON.parse(window.localStorage.getItem('jokes') || '[]'), 
+    loading: false
+  };
     this.handleClick = this.handleClick.bind(this);
   }
   componentDidMount() {
@@ -25,7 +27,7 @@ class JokeList extends React.Component {
       });
       jokes.push({ id: uuid(), text: res.data.joke, votes: 0 });
     }
-    this.setState(st => ({jokes: [...st.jokes, ...jokes]}),
+    this.setState(st => ({loading: false, jokes: [...st.jokes, ...jokes]}),
     () => window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes)));
   }
   handleVote(id, delta) {
@@ -33,10 +35,18 @@ class JokeList extends React.Component {
     () => window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes)));
   }
   handleClick() {
-    this.getJokes();
+    this.setState({loading: true}, this.getJokes);
   }
   
   render() {
+    if(this.state.loading){
+      return(
+        <div className='JokeList-spinner'>
+          <i className='far fa-8x fa-laugh fa-spin' />
+          <h1 className='Jokelist-title'>Loading...</h1>
+        </div>
+      );
+    }
     return(
       <div className='JokeList'>
         <div className='JokeList-sidebar'>
