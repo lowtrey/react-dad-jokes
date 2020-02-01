bd64473cf1628c07cb5c6ac0be6bd9fe3778a07c
Q. Price <priceqp@gmail.com>
2020-02-01 07:23:16 -0600

    Update dependencies

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import React from 'react';
-import JokeList from './JokeList';
-import './App.css';
+import React from "react";
+import JokeList from "./JokeList";
+import "./App.css";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <JokeList />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <JokeList />
+  </div>
+);
 
 export default App;
